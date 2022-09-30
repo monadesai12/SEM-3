414c95e656db1c6ef83c4e0ae6aa5d0b103db2c9
monadesai12 <85950235+monadesai12@users.noreply.github.com>
2022-09-30 04:57:21 -0700

    Add files via upload

---
+++ MiniProject-Robotics/MiniProject/src/robotics/MiniProject.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package robotics;
+
+/**
+ *
+ * @author Admin
+ */
+import ch.aplu.robotsim.*;
+import java.util.Scanner;
+public class MiniProject {
+
+    MiniProject(){
+        LegoRobot r=new LegoRobot();
+        Gear g = new Gear();
+        LightSensor ls= new LightSensor(SensorPort.S3);
+        r.addPart(g);
+        r.addPart(ls);
+        g.forward();
+        g.setSpeed(100);
+        while(true){
+        int v =ls.getValue();
+
+        if(v >= 1000)
+        g.forward();
+        if(v < 1000)
+        g.left();
+        if(v < 100)
+        g.stop();
+
+    }
+    }
+    static {
+        RobotContext.setStartPosition(430,230);
+        RobotContext.useBackground("sprites/yellowpath.gif");
+    }
+    public static void main (String args[]){
+        new MiniProject();
+    }
+    
+   
+      
+ 
+}
+
